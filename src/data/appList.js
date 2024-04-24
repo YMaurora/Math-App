825236aa490dae535a71757d920920232285e312
Aurora Yam <agym1997ny@hotmail.com>
2024-04-24 00:38:40 -0600

    Agregue nuevos datos

--- src/data/appList.js
+++ src/data/appList.js
@@ -1,10 +1,10 @@
 export const appList = [
     {
-        "title": "FastMath-Tomar Foto & Resolver",
+        "title": "FastMath",
         "avatarUrl": "https://sg-res.9appsdownloading.com/sg/res/jpg/a4/f4/7058be3c92728636f57da687fcd8-50k.jpg?x-oss-process=style/mq200",
         "avatarFallback": "FM",
         "description": "Resuelve problemas de matemáticas con AI, cubriendo desde aritmética hasta cálculo. Proporciona explicaciones paso a paso, gráficos y videos. Perfecto para tareas.",
-        "appUrl": "https://apps.apple.com/es/app/fastmath-tomar-foto-resolver/id1438642238",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.ittentechnologies.takephotoandsolvefastmath&hl=es_MX&gl=US",
         "excercisePath": "ejercicios/app-1"
     },
     {
@@ -19,7 +19,7 @@ export const appList = [
         "title": "Cymath",
         "avatarUrl": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/50/2d/f0/502df0ba-f921-64b3-f8cf-3047236214cd/AppIcon-1x_U007emarketing-0-6-0-0-85-220-0.png/512x512bb.jpg",
         "avatarFallback": "CY",
-        "description": "Solucionador matemático usa IA y heurística para resolver problemas paso a paso, como un profesor.",
+        "description": "Solucionador matemático online que usa IA y heurística para resolver problemas paso a paso, como un profesor.",
         "appUrl": "https://www.cymath.com/sp",
         "excercisePath": "ejercicios/app-3"
     },
@@ -28,135 +28,135 @@ export const appList = [
         "avatarUrl": "https://play-lh.googleusercontent.com/swH8ta0_-CeWr7vK_Q6vuNA-ghE3g5w9RkXf6EPM29cUdfAzny0fM0NeZGcTbcHNSg",
         "avatarFallback": "PS",
         "description": "Escanear, resolver, aprender. Sustituye tutores costosos, apoyo en todo tipo de preguntas. Revisión de tareas con explicaciones detalladas.",
-        "appUrl": "https://picsolve.ai/",
-        "excercisePath": ""
+        "appUrl": "https://play.google.com/store/apps/details?id=ai.picsolve&hl=es_MX&gl=US",
+        "excercisePath": "ejercicios/app-4"
     },
     {
-        "title": "SnapCalc",
-        "avatarUrl": "https://images.sftcdn.net/images/t_app-icon-m/p/f71449c2-db37-11e7-b1fb-efff2f472666/159517011/snapcalc-math-problem-solver-logo",
+        "title": "Algebrator",
+        "avatarUrl": "https://play-lh.googleusercontent.com/S7HN8Jvwyekuh4o4FSCzFX8XcUR6T6aBdW36KJimdcucM2tNGjzDo9qobHa0CmQh1A",
         "avatarFallback": "SC",
-        "description": "Haz una foto de tu problema matemático y obtén la respuesta. Soluciona desde álgebra hasta cálculo, incluso manuscrito.",
-        "appUrl": "https://apps.apple.com/es/app/snapcalc-calculadora-de-fotos/id1267331464",
-        "excercisePath": ""
+        "description": "Algebrator, una app de matemáticas con más de 30 años de experiencia, ofrece calculadoras de álgebra, científica y fracciones. Ayuda a estudiantes de secundaria y universidad a resolver problemas proporcionando pasos de solución y explicaciones detalladas. Ideal para hacer tareas rápidamente.",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.softmath.algebrator",
+        "excercisePath": "ejercicios/app-5"
     },
     {
         "title": "MathPapa",
-        "avatarUrl": "",
+        "avatarUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScpw304xxGLmlBnVcM076Xjbazek57bCKQGsqzlyOrBQ&s",
         "avatarFallback": "MP",
-        "description": "",
-        "appUrl": "/",
-        "excercisePath": ""
+        "description": "Aprende álgebra paso a paso. Simplemente escriba su problema en el cuadro de texto.",
+        "appUrl": "https://www.mathpapa.com/",
+        "excercisePath": "ejercicios/app-6"
     },
     {
         "title": "Photomath",
-        "avatarUrl": "",
+        "avatarUrl": "https://educaciondigital.unnoba.edu.ar/wp-content/uploads/unnamed-4.png",
         "avatarFallback": "PM",
-        "description": "",
-        "appUrl": "/",
-        "excercisePath": ""
+        "description": "Es una aplicación móvil descrita como una calculadora por cámara, que utiliza la cámara del teléfono móvil para reconocer patrones matemáticos y mostrar la solución directamente en la pantalla. ",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.microblink.photomath&hl=es&gl=US",
+        "excercisePath": "ejercicios/app-7"
     },
     {
         "title": "El Rey de las Matemáticas ",
-        "avatarUrl": "",
+        "avatarUrl": "https://www3.gobiernodecanarias.org/medusa/ecoescuela/recursosdigitales/files/formidable/icon_with_shadow_531.png",
         "avatarFallback": "RM",
-        "description": "/",
-        "appUrl": "",
+        "description": "Juego dinámico, muy divertido y con múltiples problemas sobre diferentes temas matematicos.",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.oddrobo.komj&hl=es&gl=US",
         "excercisePath": ""
     },
     {
         "title": "Microsoft Math SOLVER",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/WXGMGESfR30mudVSYqs_pfqaKmbv48KPnDUZx1OrYmKN4t3CpgLikc9zwm_hh-wUMyNM",
         "avatarFallback": "MM",
-        "description": "/",
-        "appUrl": "",
-        "excercisePath": ""
+        "description": "Este solucionador matemático avanzado con IA proporciona una herramienta integral para resolver problemas matemáticos de manera efectiva, facilitando el aprendizaje y la comprensión de conceptos difíciles. ",
+        "appUrl": "https://math.microsoft.com/es",
+        "excercisePath": "ejercicios/app-9"
     },
     {
         "title": "Calculadora gráfica + Math",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/n0DtVN-JUgZSxHy49trC3-WXpTVRe1_tOVdo-ee0kBOSVbqqI4FiM4bhU0qy70YF6uY=w240-h480-rw",
         "avatarFallback": "CG",
-        "description": "/",
-        "appUrl": "",
-        "excercisePath": ""
+        "description": "Una herramienta de aprendizaje innovadora para matemáticas, utilizada globalmente por profesores, ofrece una enseñanza interesante y efectiva. Disponible para uso doméstico, asegura la resolución rápida y efectiva de tareas, promoviendo la retención del conocimiento.",
+        "appUrl": "https://play.google.com/store/apps/details?id=us.mathlab.android&hl=es&gl=US",
+        "excercisePath": "ejercicios/app-10"
     },
     {
         "title": "GeoGebra",
-        "avatarUrl": "",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Geogebra.svg/1200px-Geogebra.svg.png",
         "avatarFallback": "GE",
-        "description": "/",
-        "appUrl": "",
+        "description": "El software interactivo combina geometría, álgebra, estadística y cálculo, siendo aplicable en física, proyecciones comerciales, estimaciones de decisiones estratégicas y diversas disciplinas.",
+        "appUrl": "https://www.geogebra.org/?lang=es",
         "excercisePath": ""
     },
     {
         "title": "Smartick",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/S_J6j_4P8ieE6c-kO4vhXcVQpTYtYdHaTapcSHSuiYIrYaVaYCZbdXZ0sprR-ffrag",
         "avatarFallback": "ST",
-        "description": "/",
-        "appUrl": "",
+        "description": "Es un método online para que los niños de 4 a 14 años aprendan y dominen las matemáticas, desde su casa, y dedicando sólo 15 minutos cada día. Para usar Smartick solo se necesita un PC o una Tablet con conexión a Internet.",
+        "appUrl": "https://mx.smartickmethod.com/",
         "excercisePath": ""
     },
     {
         "title": "Fórmulas Free",
-        "avatarUrl": "",
+        "avatarUrl": "https://recursos.educared.fundaciontelefonica.com.pe/recursos/ciencia/068.png",
         "avatarFallback": "FF",
-        "description": "/",
-        "appUrl": "",
+        "description": "Proporciona tiene muchas herramientas para calcular las formas geométricas o de encontrar las raíces de las ecuaciones.",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.nsc.pf.free",
         "excercisePath": ""
     },
     {
         "title": "Socratic",
-        "avatarUrl": "/",
+        "avatarUrl": "https://play-lh.googleusercontent.com/vCFjyJz8fmJTR0wCJik4p2ZwM-UUkdXmBQadKDxQQWcLWNn6zHjTXUknm9HahBVBq_AJ",
         "avatarFallback": "SO",
-        "description": "",
-        "appUrl": "",
-        "excercisePath": ""
+        "description": "Es una app de Google, ayuda a estudiantes universitarios y de bachillerato a entender su trabajo académico. Utilizando inteligencia artificial y búsqueda de Google, conecta a los alumnos con recursos educativos en la web y YouTube para facilitar el aprendizaje.",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.google.socratic&hl=es&gl=US",
+        "excercisePath": "ejercicios/app-14"
     },
     {
-        "title": "SAMARTH MATHEMATICS ",
-        "avatarUrl": "",
+        "title": "Samarth Mathematics ",
+        "avatarUrl": "https://play-lh.googleusercontent.com/1xSEKqAkC4SaKHCA-6cwNvJn9Rthm7vx-z38_FNXn5NV3wJmRk7iUHFu7Leej4JvgQ=w240-h480-rw",
         "avatarFallback": "SM",
-        "description": "/",
-        "appUrl": "",
+        "description": " Plataforma en línea para tutorías con gestión eficiente de clases, asistencia, tarifas, tareas y reportes detallados de rendimiento. Ideal para padres que desean monitorear el progreso académico de sus hijos.",
+        "appUrl": "https://play.google.com/store/apps/details?id=co.shield.kiikr&hl=es_419&gl=US",
         "excercisePath": ""
     },
     {
         "title": "MexAlgebra.xyz",
         "avatarUrl": "",
         "avatarFallback": "MA",
-        "description": "/",
-        "appUrl": "",
+        "description": "Plataforma web con más de 500 ejercicios que promueven el sentido del número y de la estructura del álgebra escolar",
+        "appUrl": "https://www.mexalgebra.xyz/home",
         "excercisePath": ""
     },
     {
         "title": "MalMath",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/f18C6GM-AAjO8oUxnp_bUZibPV50yJRyj2vjYa3Kx0CwSLy51cJad1RCOsGB1RfhMxQ=w240-h480-rw",
         "avatarFallback": "MM",
-        "description": "/",
-        "appUrl": "",
+        "description": "Solucionador de problemas de matemáticas con descripción paso a paso y la vista gráfica. Es gratis y funciona sin conexión a internet.",
+        "appUrl": "https://play.google.com/store/apps/details?id=com.malmath.apps.mm&hl=es_MX&gl=US",
         "excercisePath": ""
     },
     {
         "title": "iMathematics!",
-        "avatarUrl": "",
+        "avatarUrl": "https://www.eastersealstech.com/wp-content/uploads/2020/01/imathematics-app.png",
         "avatarFallback": "IM",
-        "description": "/",
-        "appUrl": "",
+        "description": "App ayudante y solucionador de matemáticas, diseñada para iPad. Excelente en resolver problemas de matemáticas elementales, como la aritmética, el álgebra, la geometría y el cálculo.",
+        "appUrl": "https://apps.apple.com/gb/app/imathematics/id337535181?l=es",
         "excercisePath": ""
     },
     {
         "title": "QANDA",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/p6WEOLlV2sp8eIWPUh54DblDbCEsL88ix2m2SdxUYJs4M_94YYoRu40IiY3yf5GY1Q=w240-h480-rw",
         "avatarFallback": "QA",
-        "description": "/",
-        "appUrl": "",
+        "description": "Es un solucionador de matemáticas, donde puedes tomar una foto de tu problema matemático o de tu ecuación y te da la solución explicada paso a paso.",
+        "appUrl": "https://qanda.ai/es",
         "excercisePath": ""
     },
     {
         "title": "Gauthmath",
-        "avatarUrl": "",
+        "avatarUrl": "https://play-lh.googleusercontent.com/1CnI8ztueH5zyg2PLN1qEcPaL_j56Y48gEr2OKcD31ce799Qw91_U0QneDLmwYlCo3w",
         "avatarFallback": "GM",
-        "description": "/",
-        "appUrl": "",
-        "excercisePath": ""
-    },
+        "description": "Es una app para resolver problemas matemáticos con respuestas instantáneas y acceso a tutores. Ahorra tiempo, brinda retroalimentación inmediata y motiva a los estudiantes, aunque no reemplaza el aprendizaje manual y la comprensión profunda de los conceptos.",
+        "appUrl": "https://www.gauthmath.com/",
+        "excercisePath": "ejercicios/app-20"
+    },  
 ]
--- src/data/ejerciciosList.js
+++ src/data/ejerciciosList.js
@@ -1,7 +1,7 @@
 export const ejerciciosList = [
     {
         key:"app-1",
-        name:"FastMath-Tomar Foto & Resolver",
+
         examplesImageOfUseApp:[
             {
                 path:"/exampleimg.jpg",
@@ -15,83 +15,344 @@ export const ejerciciosList = [
                 path:"/exampleimg.jpg",
                 alt:"img 3"
             },
+
         ],
+        name:"FastMath",
         list:[
+            
             {
-                type:"Calculo integral",
+                type:"Algebra",
                 exercise:[
-                    "ejercicio 1",
-                    "ejercicio 2",
-                    "ejercicio 3",
-                    "ejercicio 4",
+                    "Ejercicio 1:",
+                    "Resolver la siguiente expresión: 24*3+12/4",
+                    "El resultado de la operacion 24*3+12/4 debe de dar 75",
+                    "",
+                    "Ejercicio 2:",
+                    "Resolver la siguiente expresión: 3x^2 - 12",
+                    ""
                 ]
             },
             {
-                type:"Calculo Diferencial",
+                type:"Calculo",
                 exercise:[
-                    "ejercicio 1",
-                    "ejercicio 2",
-                    "ejercicio 3",
-                    "ejercicio 4",
+                    "Ejercicio 1: "
                 ]
-            }
+            },
         ]
 
     },
     {
         key:"app-2",
-        name:"Mathway",
         examplesImageOfUseApp:[
-            {
-                path:"/exampleimg.jpg",
-                alt:"img 1"
-            },
+
         ],
+        name:"Mathway",
         list:[
             {
-                type:"Calculo integral",
+                type:"Algebra",
                 exercise:[
-                    "ejercicio 1",
-                    "ejercicio 2",
-                    "ejercicio 3",
-                    "ejercicio 4",
+                    "Ejercicio 1: Resolver la siguiente expresión: 3a^2+6a+8a^2",
+                    "Resultado esperado: 11a^2+6a",
                 ]
             },
-            {
-                type:"Calculo Diferencial",
-                exercise:[
-                    "ejercicio 1",
-                    "ejercicio 2",
-                    "ejercicio 3",
-                    "ejercicio 4",
-                ]
-            }
         ]
 
     },
     {
         key:"app-3",
+        examplesImageOfUseApp:[
+
+        ],
         name:"Cymath",
-        examplesImageOfUseApp:[],
         list:[
             {
-                type:"Calculo integral",
+                type:"Algebra",
                 exercise:[
-                    "ejercicio 1x",
-                    "ejercicio 2x",
-                    "ejercicio 3x",
-                    "ejercicio 4x",
+                    "Ejercicio 1: Resolver la siguiente ecuación: x+5=20",
+                    "Resultado esperado: El valor de x que satisface la ecuación x + 5 = 20 es x = 15.",
+                    "",
+                    "Ejercicio 2: y=2x+5",
+                    "El resultado esperado es: y = 2x + 5 para x es x = y - 5/2.",
                 ]
             },
+        ]
+
+    },
+    {
+        key:"app-4",
+        examplesImageOfUseApp:[
+
+        ],
+        name:"PicSolve",
+        list:[
             {
-                type:"Calculo Diferencial",
+                type:"Algebra",
                 exercise:[
-                    "ejercicio 1x",
-                    "ejercicio 2x",
-                    "ejercicio 3x",
-                    "ejercicio 4x",
+                    "Ejercicio 1: 2x + 5 = 17",
+                    "Resultado esperado: 2x + 5 = 17 es x = 6", 
+                    "",
+                    "Ejercicio 2: Calcula el área de un triángulo con base 5 unidades y altura 8 unidades.",
+                    "Resultado esperado: el área del triángulo con base de 5 unidades y altura de 8 unidades es 20 unidades cuadradas."
                 ]
-            }
+            },
+        ]
+
+    },
+    {
+        key:"app-5",
+        examplesImageOfUseApp:[
+
+        ],
+        name:"Algebrator",
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: Resuelve la siguiente ecuación (3x + 9 = 21)",
+                    "Resultado esperado: la solución de la ecuación (3x + 9 = 21) es (x = 4).",
+                    "",
+                    "Ejercicio 2: Resuelve la siguiente ecuacion (4y - 5 = 7)",
+                    "La solución de la ecuación (4y - 5 = 7) es (y = 3).",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-6",
+        examplesImageOfUseApp:[
+
+        ],
+        name:"MathPapa",
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: 3x + 5 = 20",
+                    "Resultado esperado: 3x + 5 = 20 es x = 5."
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-7",
+        examplesImageOfUseApp:[
+
+        ],
+        name:"Photomath",
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: y = 5x",
+                    "Nota: Toma la foto al ejercicio",
+                    "Resultado esperado: y = 5x para x es x = y/5.",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-9",
+        examplesImageOfUseApp:[
+
+        ],
+        name:"Microsoft Math SOLVER",
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: 12a + 3 = 9",
+                    "Resultado de la ecuación: 12a + 3 = 9 es a = 1/2.",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-10",
+        name:"Calculadora gráfica + Math",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: y = 2x + 3",
+                    "Solucion de la ecuación: y = 2x + 3 para x es x=y - 3/2.",
+                    "",
+                    "Ejercicio 2: X^2-4x+4=0",
+                    "Solución de la ecuación x^2 - 4x + 4 = 0 es x = 2"
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-11",
+        name:"GeoGebra",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-12",
+        name:"Smartick",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-13",
+        name:"Fórmulas Free",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-14",
+        name:"Socratic",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1: 3x-10=5",
+                    "Solución de la ecuación 3x - 10 = 5 es x = 5.",
+                    "",
+                    "Ejercicio 2: 4y+8=20 ",
+                    "Solución de la ecuación 4y + 8 = 20 es y = 3."
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-15",
+        name:"Samarth Mathematics",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-16",
+        name:"MexAlgebra.xyz",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-17",
+        name:"MalMath",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-18",
+        name:"iMathematics!",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-19",
+        name:"QANDA",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "",
+                ]
+            },
+        ]
+
+    },
+    {
+        key:"app-20",
+        name:"Gauthmath",
+        examplesImageOfUseApp:[
+
+        ],
+        list:[
+            {
+                type:"Algebra",
+                exercise:[
+                    "Ejercicio 1:",
+                    "Escanea con tu celular el ejercicio",
+                    "Solución de la ecuación: 3y + 6 = 15 es y = 3."
+                ]
+            },
         ]
 
     }
